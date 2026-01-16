f92a74ceddd3f8e6c8c097bc29ce9d93f6a40112
Shekhar Rana <119071318+shekharrana007@users.noreply.github.com>
2026-01-16 14:58:02 +0530

    Time: 0 ms (100%), Space: 14.8 MB (97.83%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -5,16 +5,26 @@ public:
         int s=0;
         int n=mat.size();
 
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(i==j){
+        //             p+=mat[i][j];
+        //         }
+        //        else  if((i+j)==(n-1)){
+        //             s+=mat[i][j];
+        //         }
+        //     }
+        // }
+        int mid=n/2;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i==j){
-                    p+=mat[i][j];
-                }
-               else  if((i+j)==(n-1)){
-                    s+=mat[i][j];
-                }
-            }
+            
+            p+=mat[i][i];
+            s+=mat[i][n-i-1];
+            
         }
+        if(n%2==1){
+               s= s-mat[mid][mid];
+            }
         int ans=p+s;
         return ans;
     }
