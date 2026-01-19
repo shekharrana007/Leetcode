10ec3b11a7a8f8b73e147b404a3e1b53cab0cd57
Shekhar Rana <119071318+shekharrana007@users.noreply.github.com>
2026-01-19 15:16:49 +0530

    Time: 0 ms (100%), Space: 10 MB (5.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,65 +1,25 @@
 class Solution {
 public:
     bool isValid(string s) {
-        // stack<char>st;
-        // for(int i=0;i<s.size();i++){
-        //     char ch=s[i];
-        //     if(ch=='('||ch=='['||ch=='{'){
-        //         st.push(ch);
-        //     }
-        //     else{
-        //         if(!st.empty()){
-        //             char top=st.top();
-        //             if(ch==')' && top=='('||ch==']' && top=='['||ch=='}' && top =='{'){
-        //              st.pop();
-        //             }
-        //             else{
-        //                 return false;
-        //             }
-        //         }
-        //         else{
-        //             return false;
-        //         }
-        //     }
-        // }
-        // if(st.empty()){
-        //     return true;
-        // }
-        // else{
-        //     return false;
-        // }
-
-
-
-
-
-        stack<char>st;
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            char ch=s[i];
-            if(ch=='('||ch=='['||ch=='{'){
-                st.push(ch);
+      
+         
+        stack<int>st;
+        for(char c:s){
+            if(c=='('||c=='['||c=='{'){
+                st.push(c);
             }
             else{
-                if(!st.empty()){
-                    char top =st.top();
-                    if(ch==')' && top=='(' || ch==']'&& top=='[' ||ch=='}' && top=='{'){
-                        st.pop();
-                    }
-                    else{
-                        return false;
-                    }
+                if(st.empty())return false;
+                char d=st.top();
+                if(c==')'&&d=='('||c==']'&& d=='['||c=='}'&& d=='{'){
+                    st.pop();
                 }
                 else{
                     return false;
                 }
             }
         }
-        if(st.empty()){
-            return true;
-        }
-        else{
-            return false;
-        }
+        
+        return st.empty();
     }
 };
